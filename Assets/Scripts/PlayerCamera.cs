bc7f7ca809e5bb4825708bb713762c5a1b6b5015
Carita Furstenborg <carita.furstenborg@gmail.com>
2018-08-12 10:26:22 +0300

    Add stuff, propably important

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -8,24 +8,32 @@ namespace Odiana
 	{
 		private int playerId;
 		private Transform player;
-		private Vector3 minCamPos;
-		private Vector3 maxCamPos;
+		private float xMax, xMin, yMax, yMin;
 
 		void Awake ()
 		{
 			player = FindObjectOfType<Player>().transform;
+			Vector3 minTile = new Vector3(-15.5f, -37.5f, 0f);
+			Vector3 maxTile = new Vector3(29, 7, 0);
+			SetLimits(minTile, maxTile);
 		}
 
 		void LateUpdate()
 		{
-			//transform.position = new Vector3(Mathf.Clamp(player, minCamPos, maxCamPos));
+			transform.position = new Vector3(Mathf.Clamp(player.position.x, xMin, xMax), Mathf.Clamp(player.position.y, yMin, yMax), -10f);
 		}
 
-		private void SetLimits(Vector3 minPos, Vector3 maxPos)
+		private void SetLimits(Vector3 minTile, Vector3 maxTile)
 		{
 			Camera cam = Camera.main;
-			minCamPos = minPos;
-			maxCamPos = maxPos;
+			
+			float height = 2f * cam.orthographicSize;
+			float width = height * cam.aspect;
+
+			xMin = minTile.x + width / 2;
+			xMax = maxTile.x - width / 2;
+			yMax = maxTile.y - height / 2;
+			yMin = minTile.y + height / 2; 
 		}
 	}
 }
